e656689506d2290d942b15f068d1988c049fbd89
FousiyaPH <fousiyanizarali@gmail.com>
2023-10-16 20:12:08 +0100

    added comments

--- code/ec2.tf
+++ code/ec2.tf
@@ -3,12 +3,15 @@ provider "aws" {
     version = "~>2.70"
 }
 
+# after exceuting this code try to execute by changing the instance name (vm-3)- will terminate the vm-2 instance
 resource "aws_instance" "vm-2" { 
     ami = "ami-0b9fd8b55a6e3c9d5"
     instance_type = "t2.micro"
   
 }
 
+# removing below code and exceuting(terraform apply) will not destoy the EC2 resource
+# but will be destroy the configuration for the below code from the tfstate file
 
 resource "aws_eip" "lb" {
     vpc = true
