70fc012b74ec0f0ae898121bedb89694968782fd
FousiyaPH <fousiyanizarali@gmail.com>
2023-10-17 22:58:10 +0100

    creating multiple user by using count parameter

--- terraform-01/myfirstec2.tf
+++ terraform-01/myfirstec2.tf
@@ -8,3 +8,17 @@ resource "aws_instance" "myfirstec2-2" {
     Name = var.instance_name
   }
 }
+
+#The problem with the below code is that all the 5 IAM user will have the same name
+resource "aws_iam_user" "user" {
+  name  = fousiya
+  count = 5
+}
+
+#count.index  allows us to fetch the index of each iteration in the loop
+# The below code will be create 5 IAM users named as loadbalancer.0,loadbalancer.1,loadbalancer.2,loadbalancer.3,loadbalancer.4 
+
+resource "aws_iam_user" "user" {
+  name  = "loadbalancer.${count.index}"
+  count = 5
+}
