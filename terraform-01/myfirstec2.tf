dcb864657d1b7cad8386b8a50f5d381b095a8c6b
FousiyaPH <fousiyanizarali@gmail.com>
2023-10-17 23:44:11 +0100

    changes added and error fixed

--- terraform-01/myfirstec2.tf
+++ terraform-01/myfirstec2.tf
@@ -10,15 +10,17 @@ resource "aws_instance" "myfirstec2-2" {
 }
 
 #The problem with the below code is that all the 5 IAM user will have the same name
+# while executing terraform plan command will show the error creating IAM User (Aina): EntityAlreadyExists: User with name Aina already exists
+# create one user named Aina
 resource "aws_iam_user" "user" {
-  name  = fousiya
+  name  = "Aina"
   count = 5
 }
 
 #count.index  allows us to fetch the index of each iteration in the loop
 # The below code will be create 5 IAM users named as loadbalancer.0,loadbalancer.1,loadbalancer.2,loadbalancer.3,loadbalancer.4 
 
-resource "aws_iam_user" "user" {
+resource "aws_iam_user" "user-1" {
   name  = "loadbalancer.${count.index}"
   count = 5
 }
