8810369f7aaa9a14da9b3fbae691a1651b3a1814
FousiyaPH <fousiyanizarali@gmail.com>
2023-10-25 22:54:34 +0100

    policy added

--- terraform-03/s3files.tf
+++ terraform-03/s3files.tf
@@ -11,9 +11,29 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+# creating bucket
 resource "aws_s3_bucket" "mys3bucket" {
-  bucket  = var.s3_bucket_names[count.index]
-  count = 5
+  for_each  = var.s3_bucket_names
+  bucket = each.key
 }
 
+
+resource "aws_s3_bucket_policy" "deny_delete_bucket" {
+  for_each = aws_s3_bucket.mys3bucket
+
+  bucket = each.key
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "MYBUCKETPOLICY",
+    Statement = [
+      {
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:DeleteBucket"
+        Resource  = each.value.arn
+        
+      }
+    ]
+  })
+}
 
